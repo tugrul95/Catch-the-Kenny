4f0fedf47f22ca85338a1f022a82fbc59c6e5ece
tugrul95 <tugrul1995@gmail.com>
2021-12-28 14:13:02 +0300

    Initial Commit

---
+++ Ödev-kenny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ödev-kenny
+//
+//  Created by Macbook on 28.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
