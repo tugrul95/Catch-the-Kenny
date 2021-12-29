7e61f0e68fd579101a6dd796d5238b3e3f75a415
tugrul95 <tugrul1995@gmail.com>
2021-12-29 14:15:28 +0300

    first commit

--- Ödev-kenny/ViewController.swift
+++ Ödev-kenny/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var timer = Timer()
+    let imageViewWidth = CGFloat(100)
+    let imageViewHeight = CGFloat(100)
+    var highScore = 0
+    var score = 0
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var highScoreLabel: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        SchedulingMethod()
+        scoreLabel.text = "Score: \(score)"
+        
+}
+    func SchedulingMethod() {
+        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(updatePosition), userInfo: nil, repeats: true)
+       }
+       @objc func updatePosition() {
 
+           let maxX = view.frame.maxX - imageViewWidth
+           let maxY = view.frame.maxY - imageViewHeight
+           let xCoord = CGFloat.random(in: 0...maxX)
+           let yCoord = CGFloat.random(in: 0...maxY)
+
+           UIView.animate(withDuration: 0.3) {
+               self.imageView.transform = CGAffineTransform(translationX: xCoord, y: yCoord)
+           }
 
 }
-
+}
